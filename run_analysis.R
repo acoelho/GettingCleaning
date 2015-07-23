749ced4bde46e579ecb4dd6f28e76e69ab9bd770
acoelho <coelho.amp@gmail.com>
2015-07-22 18:54:37 -0700

    data loading , some early processing

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
-library(plyr)
-library(curl)
+library(dplyr)
+
 ##download, unzip data
 
 if (!file.exists("./getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")) {
@@ -7,17 +7,32 @@ if (!file.exists("./getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")) {
   download.file(fURL,destfile="getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")
 }
 
-unzip("./getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")
+flist<- unzip("getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", list=TRUE)$Name
+
+#unzip("./getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip")
 
 ##load data into data frames
 
-testData <- read.table("./UCI HAR Dataset/test/X_test.txt")
-testY <- read.table("./UCI HAR Dataset/test/Y_test.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
+
+testData <- read.table("./UCI HAR Dataset/test/X_test.txt", col.names = features[,2])
+testY <- read.table("./UCI HAR Dataset/test/y_test.txt")
 testSub <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-trainData <- read.table("./UCI HAR Dataset/train/X_train.txt")
 
+test <- mutate(testData, Subject = testSub[,1], Activity = testY[,1])
 
-fulData <- rbind(testData, trainData)
+trainData <- read.table("./UCI HAR Dataset/train/X_train.txt", col.names = features[,2])
+trainY <- read.table("./UCI HAR Dataset/train/y_train.txt")
+trainSub <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+train <- mutate(trainData, Subject = trainSub[,1], Activity = trainY[,1])
+
+fulData <- rbind(test, train)
 ##
+
+selFeatures <- grep("mean\\(\\)|std\\(\\)", features[, 2])
+selFeatures <- c(selFeatures,562,563)
+
+
 
 #mutate
